4f85fab1d61d16b05051b52cbee86389374441c0
Jon Davis <jon@jondavis.net>
2012-09-26 14:32:39 -0700

    Update namespace.js

--- namespace.js
+++ namespace.js
@@ -1,19 +1,14 @@
-// Author: Jon Davis <jon@jondavis.net>
-// Version: 1.0.1
+// Author: steve <sjakubowsi@hotmail.com>
+// Maintainer: Jon Davis <jon@jondavis.net>
+// Version: 1.1
 // https://github.com/stimpy77/namespace.js
 
-namespace = function(n) {
-    var s = n.split('.');
-    var exp = 'var ___v=undefined;try {___v=x} catch(e) {} if (___v===undefined)x={}';
-    var e = exp.replace(/x/g, s[0]);
-    eval(e);
-    for (var i=1; i<s.length; i++) {
-        var ns = '';
-        for (var p=0; p<=i; p++) {
-            if (ns.length > 0) ns += '.';
-            ns += s[p];
-        }
-        e = exp.replace(/x/g, ns);
-        eval(e);
+function namespace(ns) {
+    var g = function(){return this}();
+    ns = ns.split('.');
+    for(var i=0, n=ns.length; i<n; ++i) {
+        var x = ns[i];
+        if (x in g === false) g[x]={}; 
+        g = g[x];
     }
-}
+} 
