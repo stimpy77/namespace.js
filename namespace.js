6b745fcb432d358aa6be63606cc6eecde68417f4
Jon Davis <jon@jondavis.net>
2013-01-10 05:16:53 -0700

    Update namespace.js
    Add object assignment and WinJS support.

--- namespace.js
+++ namespace.js
@@ -1,14 +1,21 @@
-// Author: steve <sjakubowsi@hotmail.com>
-// Maintainer: Jon Davis <jon@jondavis.net>
-// Version: 1.1
+// Version: 1.2
 // https://github.com/stimpy77/namespace.js
 
-function namespace(ns) {
+function namespace(ns, obj) {
     var g = function(){return this}();
+    if (g['WinJS'] && WinJS.Namespace) { // Windows 8 apps have namespaces
+        return WinJS.Namespace.define(ns, obj);
+    }
     ns = ns.split('.');
     for(var i=0, n=ns.length; i<n; ++i) {
         var x = ns[i];
-        if (x in g === false) g[x]={}; 
+        if (x in g === false) {
+            if (obj !== undefined && i == ns.length-1) {
+                g[x] = obj;
+            } else {
+                g[x]={}; 
+            }
+        }
         g = g[x];
     }
 } 
