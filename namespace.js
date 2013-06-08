c9f9d9c35aedf590d3641a4e2b4edcb8fac96010
Jon Davis <jon@jondavis.net>
2013-06-08 14:49:24 -0600

    Update namespace.js
    Remove WinJS implementation; if WinJS then also YUI, dojo, etc. No.

--- namespace.js
+++ namespace.js
@@ -10,17 +10,11 @@ function namespace(nss, obj) {
     var exist = (x in g === true);
 
     if (!exist) {
-      if (isWinJS) {
-        if (i == 0) WinJS.Namespace.define(x, ns.length > 1 ? undefined : obj);
-        else if (i != ns.length - 1) WinJS.Namespace.defineWithParent(g, x);
-        else return WinJS.Namespace.defineWithParent(g, x, obj);
-      } else {
         if (i == ns.length - 1 && obj !== undefined && obj !== null) {
           g[x] = obj;
         } else {
           g[x] = {};
         }
-      }
     }
     g = g[x];
   }
