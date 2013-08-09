8c71624b4aeb5092582060f7d31d171cb5d7d4f4
Jon Davis <jon@jondavis.net>
2013-08-09 10:48:04 -0700

    Update namespace.js

--- namespace.js
+++ namespace.js
@@ -1,9 +1,8 @@
-// Version: 1.2
+// Version: 1.3
 // https://github.com/stimpy77/namespace.js
 
 function namespace(nss, obj) {
-  var g = function () { return this ? this : window; }();
-  var isWinJS = !(!(g['WinJS'])) && !(!(WinJS.Namespace));
+  var g = (function () { return this ? this : window; }).call(this);
   var ns = nss.split('.');
   for (var i = 0, n = ns.length; i < n; i++) {
     var x = ns[i];
